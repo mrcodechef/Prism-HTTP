d38b4ccb94bba82b1ae849de1d0711d6b3b31f3b
YutaroHayakawa <yhayakawa3720@gmail.com>
2019-04-30 07:26:01 +0000

    Updated install request

--- install_deps.sh
+++ install_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt -y update
-apt install -y git build-essential wget automake libtool
+apt install -y git build-essential wget automake cmake libtool libssl-dev
 
 cd /root
 
@@ -9,6 +9,7 @@ wget https://github.com/libtom/tomsfastmath/releases/download/v0.13.1/tfm-0.13.1
 wget https://github.com/libtom/libtomcrypt/archive/v1.18.2.tar.gz
 wget https://github.com/libuv/libuv/archive/v1.26.0.tar.gz
 wget https://github.com/protocolbuffers/protobuf/archive/v3.6.0.1.tar.gz
+wget https://github.com/google/leveldb/archive/1.21.tar.gz
 git clone -b prism https://github.com/YutaroHayakawa/netmap
 git clone https://github.com/YutaroHayakawa/Prism-HTTP
 
@@ -16,6 +17,7 @@ tar xvf tfm-0.13.1.tar.xz
 tar xvf v1.18.2.tar.gz
 tar xvf v1.26.0.tar.gz
 tar xvf v3.6.0.1.tar.gz
+tar xvf 1.21.tar.gz
 
 cd /root/Prism-HTTP/deps
 dpkg -i linux-headers-4.18.0-prism_amd64.deb
@@ -34,17 +36,23 @@ cd /root/libuv-1.26.0
 ./autogen.sh
 ./configure
 make -j4 install
-cat /usr/local/include/uv/unix.h | sed -e 's/netinet\/tcp.h/linux\/tcp.h/g' > /usr/local/include/uv/unix.h
+cat include/uv/unix.h | sed -e 's/netinet\/tcp.h/linux\/tcp.h/g' > /usr/local/include/uv/unix.h
 
 cd /root/protobuf-3.6.0.1
 ./autogen.sh
 ./configure
 make -j4 install
 
+cd /root/leveldb-1.21
+mkdir build
+cd build
+cmake ../
+make -j4 install
+
 ldconfig
 
 cd /root/Prism-HTTP/src/proto
-./gen.sh
+bash gen.sh
 
 cd /root/Prism-HTTP
 make -j4
