8da68e9e79fdb0ce70dc93005ed7393e6a9415be
Yutaro Hayakawa <yutaro.hayakawa@linecorp.com>
2020-08-31 01:49:37 +0900

    Move the setup script to scripts

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-apt -y update
-apt install -y git build-essential wget automake cmake libtool libssl-dev pkg-config bpfcc-tools
+apt-get update
+apt-get install -y git build-essential wget automake \
+  cmake libtool libssl-dev pkg-config bpfcc-tools libbpfcc-dev libelf-dev
 
-cd /root
+# To use tcp-monitor
+cp -r /usr/src/linux-headers-4.18.0-prism3/include/uapi/linux/tcp.h /usr/include/linux/tcp.h
+
+cd $BUILD_ROOT
 
 wget https://github.com/libtom/tomsfastmath/releases/download/v0.13.1/tfm-0.13.1.tar.xz
 wget https://github.com/libtom/libtomcrypt/archive/v1.18.2.tar.gz
@@ -11,7 +15,6 @@ wget https://github.com/libuv/libuv/archive/v1.26.0.tar.gz
 wget https://github.com/protocolbuffers/protobuf/archive/v3.6.0.1.tar.gz
 wget https://github.com/google/leveldb/archive/1.21.tar.gz
 git clone -b prism https://github.com/YutaroHayakawa/netmap
-git clone https://github.com/YutaroHayakawa/Prism-HTTP
 
 tar xvf tfm-0.13.1.tar.xz
 tar xvf v1.18.2.tar.gz
@@ -19,37 +22,30 @@ tar xvf v1.26.0.tar.gz
 tar xvf v3.6.0.1.tar.gz
 tar xvf 1.21.tar.gz
 
-cd /root/Prism-HTTP/deps
-dpkg -i linux-headers-4.18.0-prism_amd64.deb
-dpkg -i linux-image-4.18.0-prism_amd64.deb
-cp -r /usr/src/linux-headers-4.18.0-prism3/include/uapi/linux/tcp.h /usr/include/linux/tcp.h
-
-cd /root/tomsfastmath-0.13.1
-make -j4
+cd $BUILD_ROOT/tomsfastmath-0.13.1
+make -j $NWORKERS
 INSTALL_GROUP=root make install
 
-cd /root/libtomcrypt-1.18.2
+cd $BUILD_ROOT/libtomcrypt-1.18.2
 make -j4 CFLAGS="-DUSE_TFM -DTFM_DESC" EXTRALIBS="-ltfm"
 make install
 
-cd /root/libuv-1.26.0
+cd $BUILD_ROOT/libuv-1.26.0
 ./autogen.sh
 ./configure
-make -j4 install
+make -j $NWORKERS install
 cat include/uv/unix.h | sed -e 's/netinet\/tcp.h/linux\/tcp.h/g' > /usr/local/include/uv/unix.h
 
-cd /root/protobuf-3.6.0.1
+cd $BUILD_ROOT/protobuf-3.6.0.1
 ./autogen.sh
 ./configure
-make -j4 install
+make -j $NWORKERS install
 
-cd /root/leveldb-1.21
+cd $BUILD_ROOT/leveldb-1.21
 mkdir build
 cd build
 cmake ../
-make -j4 install
+make -j $NWORKERS install
 
-ldconfig
-
-cd /root/Prism-HTTP/src/proto
+cd $BUILD_ROOT/Prism-HTTP/src/proto
 bash gen.sh
